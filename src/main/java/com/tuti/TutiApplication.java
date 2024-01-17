a4aadddeb4d13054442962f38cf3154aa9718b78
이정우 <acg6138@naver.com>
2024-01-17 21:09:33 +0900

    Feat: 프로젝트 세팅

---
+++ src/main/java/com/tuti/TutiApplication.java
@@ -0,0 +1,13 @@
+package com.tuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuti/TutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
