be41da5a7d13d5f37c1c90b3f6df7e7e048e1152
이정우 <acg6138@naver.com>
2024-01-24 22:51:41 +0900

    Chore: profile을 dev로 사용하도록 설정

--- src/main/java/com/tuti/auth/config/oauth/OAuthProperties.java
+++ src/main/java/com/tuti/auth/config/oauth/OAuthProperties.java
@@ -3,10 +3,12 @@ package com.tuti.auth.config.oauth;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Profile("dev")
 @ConfigurationProperties(prefix = "oauth2")
 @Getter
 public class OAuthProperties {
