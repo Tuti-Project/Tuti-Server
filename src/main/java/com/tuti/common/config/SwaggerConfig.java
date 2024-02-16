00793ce53aaddac8929dfa2cd8f12a1738846c80
이정우 <acg6138@naver.com>
2024-02-16 21:23:31 +0900

    Fix: Swagger servers 오타 수정

--- src/main/java/com/tuti/common/config/SwaggerConfig.java
+++ src/main/java/com/tuti/common/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 @OpenAPIDefinition(
         info = @Info(title = "트티 API 명세서",
                 version = "v1"),
-        servers = {@Server(url = "https://52.78.238.81:8080/", description = "Dev"),
+        servers = {@Server(url = "http://52.78.238.81:8080/", description = "Dev"),
                    @Server(url = "http://localhost:8080/", description = "Local")})
 @Configuration
 public class SwaggerConfig {
