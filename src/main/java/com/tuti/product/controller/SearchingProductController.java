228a52970d8507335a9e55d509267de5af1c502f
이정우 <acg6138@naver.com>
2024-03-01 18:44:44 +0900

    Fix: PathVariable 을 RequestParam 으로 작성되어 있던 오류 수정

--- src/main/java/com/tuti/product/controller/SearchingProductController.java
+++ src/main/java/com/tuti/product/controller/SearchingProductController.java
@@ -7,7 +7,7 @@ import com.tuti.product.service.response.ProductResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +19,7 @@ public class SearchingProductController {
 
     @Operation(summary = "상품 단건 조회")
     @GetMapping("/product/{productId}")
-    public ApiResponse<ProductDetailResponse> getProduct(@RequestParam("productId")Long productId) {
+    public ApiResponse<ProductDetailResponse> getProduct(@PathVariable("productId")Long productId) {
         return ApiResponse.ok(searchingProductService.getProduct(productId));
     }
 
